5219539345c6c626041042ef06777faf428c4114
kumarnikhil8675at-beep <kumarnikhil8675at@gmail.com>
2026-07-26 02:54:44 +0530

    timer_run_onwards

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -10,22 +9,18 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-
 window=Tk()
-# window.minsize(400,400)
 window.title("Pomodoro")
-window.
+window.config(padx=100,pady=50,bg=YELLOW)
 
 background=PhotoImage(file='tomato.png')
-# bg=Label(image=background)
-# bg.place(x=0, y=0, relwidth=1, relheight=1)
-
-canvas=Canvas(width=200,height=224,bg=YELLOW)
+canvas=Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 canvas.create_image(100,112,image=background)
-canvas.pack()
+time=canvas.create_text(100,130,text="00:00",fill="white",font=('Arial',30,'bold'))
+canvas.grid(column=2,row=2)
 
-display=Label(text="Work",font=('Arial',40))
-display.place(x=150,y=20)
+display=Label(text="Work",font=('Arial',40),bg=YELLOW,fg=GREEN)
+display.grid(column=2,row=1)
 
 watch=0
 running=False
@@ -47,15 +42,20 @@ def stopwatch():
         minutes= watch // 60
         seconds=watch % 60
             
-        time.config(text=f"{minutes:02}:{seconds:02}")
-        if minutes==25 and seconds==0 and Work_time==True:
+        canvas.itemconfig(time,text=f"{minutes:02}:{seconds:02}")
+        
+        if minutes==WORK_MIN and Work_time==True:
             Work_time=False
             display.config(text="Break",font=('Arial',40))
             finalbreak +=1
+            count=""
+            for a in range(finalbreak):
+                count +="✔"
+            howmany.config(text=count)
             watch=0
-        elif (minutes==5 or minutes==20) and seconds==0 and Work_time==False:
+        elif (minutes==SHORT_BREAK_MIN or minutes==LONG_BREAK_MIN) and Work_time==False:
             if finalbreak==4:
-                if minutes==20:
+                if minutes==LONG_BREAK_MIN:
                     Work_time=True
                     finalbreak=0
                     display.config(text="Work",font=('Arial',40))
@@ -67,22 +67,22 @@ def stopwatch():
             
         window.after(1000,stopwatch)
     
-    
 def timereset():
-    global watch,running
+    global watch,running,finalbreak
     
     watch=0
+    finalbreak=0
     running=False
-    
-    time.config(text=f"00:00")
-    
-time=Label(text="00:00",font=('Arial',20))
-time.place(x=160,y=200)
+    canvas.itemconfig(time,text=f"00:00")
+    howmany.config(text="")
 
 start=Button(text="Start",font=('Arial',20),command=timestart)
-start.place(x=100,y=320)
+start.grid(column=1,row=3)
 
 reset=Button(text="Reset",font=('Arial',20),command=timereset)
-reset.place(x=200,y=320)
+reset.grid(column=3,row=3)
+
+howmany=Label(text="",font=('Arial',20),bg=YELLOW,fg=GREEN)
+howmany.grid(column=2,row=4)
 
 window.mainloop()
